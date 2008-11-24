181116a82cf3d6b814356ef52433483a8a77e9aa
Johannes Gilger <heipei@hackvalue.de>
2008-11-24 14:12:15 +0100

    Fixes

--- get_and_combine.sh
+++ get_and_combine.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=./lib
+export LD_LIBRARY_PATH=./lib	# Dirty, doesnt work other, unless geoiplookup-libs are installed system-wide!
+
 grep -h Access /var/log/radius/radsecproxy.log*|grep -v 127.0.0.1|grep -v Reject|awk '{print $8}'|sed -e 's/.*@//'|grep -e '^.*\.\w\w$'|sort > institutions.txt
 ssh radius3 "grep -h Access /var/log/radius/radsecproxy.log*|grep -v 127.0.0.1|grep -v Reject|awk '{print $8}'|sed -e 's/.*@//'|grep -e '^.*\.\w\w$'|sed -e 's/ statio.*//'|sort" >> institutions.txt
 
-# Only extract TLD (i.e. country) from the institution's domains
-#cat institutions.txt|sed -e 's/.*\.//'|sort|uniq -c|./visitors.rb > html/countries.js
-
-#grep -h Access /var/log/radius/radsecproxy.log*|grep -v 127.0.0.1|grep -v Reject|awk '{print $8}'|sed -e 's/.*@//'|sort|sed -e 's/.*\.//'|grep -e '^\w\w$' > visitors.txt
-#ssh radius3 "grep -h Access /var/log/radius/radsecproxy.log*|grep -v 127.0.0.1|grep -v Reject|awk '{print $8}'|sed -e 's/.*@//'|sort|sed -e 's/.*\.//'|grep -e '^\w\w$'" >> visitors.txt
-
-# Also append breakdown per institution
+# Do the processing (for processing it into Javascript-readable variables)
 cat institutions.txt|sort|uniq -c|sort -r|./institutions.rb > html/countries.js
 
-#rm institutions.txt 
+rm institutions.txt 
