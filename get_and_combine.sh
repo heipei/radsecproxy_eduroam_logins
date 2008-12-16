1715bfb7b05e1949f5abe7c598f3b30da4badc36
Johannes Gilger <heipei@hackvalue.de>
2008-12-16 14:22:02 +0100

    Sort und tr getauscht

--- get_and_combine.sh
+++ get_and_combine.sh
@@ -5,6 +5,6 @@ grep -h Access /var/log/radius/radsecproxy.log*|grep -v 127.0.0.1|grep -v Reject
 ssh radius3 "grep -h Access /var/log/radius/radsecproxy.log*|grep -v 127.0.0.1|grep -v Reject|awk '{print $8}'|sed -e 's/.*@//'|grep -e '^.*\.\w\w$'|sed -e 's/ statio.*//'|sort" >> institutions.txt
 
 # Do the processing (for processing it into Javascript-readable variables)
-cat institutions.txt|sort|tr "A-Z" "a-z"|uniq -c|sort -rg|./institutions.rb > html/countries.js
+cat institutions.txt|tr "A-Z" "a-z"|sort|uniq -c|sort -rg|./institutions.rb > html/countries.js
 
 rm institutions.txt 
